aee84bdc256969a2e6739ffa09ef3792b55d357f
nintreesssss <nintreesssss@gmail.com>
2026-02-02 18:30:59 +0800

    双指针练习

---
+++ src/LinkedList/addTwoNumbers_2.java
@@ -0,0 +1,42 @@
+//LeetCode 2:两数相加
+//https://leetcode.cn/problems/add-two-numbers/description/
+
+package LinkedList;
+
+public class addTwoNumbers_2 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode sol(ListNode l1,ListNode l2){
+        ListNode p1 = l1,p2 = l2;
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        int carry = 0;
+
+        while (p1 != null || p2 != null || carry > 0) {
+            int val = carry;
+            if (p1 != null) {
+                val += p1.val;
+                p1 = p1.next;
+            }
+
+            if (p2 != null) {
+                val += p2.val;
+                p2 = p2.next;
+            }
+
+            carry = val / 10;
+            val = val % 10;
+
+            p.next = new ListNode(val);
+            p = p.next;
+        }
+
+        return dummy.next;
+    }   
+}
